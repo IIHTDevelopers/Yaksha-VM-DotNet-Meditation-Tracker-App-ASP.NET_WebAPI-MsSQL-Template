d5d6aeadf255ed0961f02c0e7eefc3a0f7445d96
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-07 18:04:37 +0530

    Add files via upload

---
+++ MeditationTrackerApp/Controllers/MeditationTrackerController.cs
@@ -0,0 +1,69 @@
+﻿using MeditationTrackerApp.DAL.Interrfaces;
+using MeditationTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MeditationTrackerApp.Controllers
+{
+    public class MeditationController : ApiController
+    {
+        private readonly IMeditationTrackerService _service;
+        public MeditationController(IMeditationTrackerService service)
+        {
+            _service = service;
+        }
+        public MeditationController()
+        {
+            // Constructor logic, if needed
+        }
+        [HttpPost]
+        [Route("api/Meditation/CreateMeditation")]
+        [AllowAnonymous]
+        public async Task<IHttpActionResult> CreateMeditation([FromBody] Meditation model)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+
+        [HttpPut]
+        [Route("api/Meditation/UpdateMeditation")]
+        public async Task<IHttpActionResult> UpdateMeditation([FromBody] Meditation model)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+
+        [HttpDelete]
+        [Route("api/Meditation/DeleteMeditation")]
+        public async Task<IHttpActionResult> DeleteMeditation(long id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+
+        [HttpGet]
+        [Route("api/Meditation/GetMeditationById")]
+        public async Task<IHttpActionResult> GetMeditationById(long id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+
+        [HttpGet]
+        [Route("api/Meditation/GetAllMeditations")]
+        public async Task<IEnumerable<Meditation>> GetAllMeditations()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MeditationTrackerApp/DAL/Services/MeditationTrackerService.cs
@@ -0,0 +1,51 @@
+﻿using MeditationTrackerApp.DAL.Interrfaces;
+using MeditationTrackerApp.DAL.Services.Repository;
+using MeditationTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MeditationTrackerApp.DAL.Services
+{
+    public class MeditationTrackerService : IMeditationTrackerService
+    {
+        private readonly IMeditationTrackerRepository _repository;
+
+        public MeditationTrackerService(IMeditationTrackerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Meditation> CreateMeditation(Meditation expense)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteMeditationById(long id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public List<Meditation> GetAllMeditations()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Task<Meditation> GetMeditationById(long id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Task<Meditation> UpdateMeditation(Meditation model)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
